ef3ab729953a77d49261a3fd38db60fc81d6dd6d
ritesh_chafer <riteshprasadsingh8@gmail.com>
2022-05-18 13:43:28 +0530

    function added for crop

--- server/server.py
+++ server/server.py
@@ -1,75 +1,83 @@
-from flask import Flask
-from requests import request
-import tensorflow as tf
-from keras.models import model_from_json, load_model
-from tensorflow.keras import backend
-from tensorflow.python.framework import ops
-import pandas as pd
+from load import *
+from flask import Flask, render_template, request
+from scipy.misc import imsave, imread, imresize
+import numpy as np
+import keras.models
+import re
+import sys
+import os
+import base64
+sys.path.append(os.path.abspath("./model"))
+
+
+global graph, model
+
+model, graph = init()
 
 app = Flask(__name__)
 
-# model paths
-model_name = "EF-3"
-models_dir = './model/models/'
-model_path = models_dir + model_name + '.json'
-model_weights_path = models_dir + model_name + '.h5'
 
-# classes/words
-classes = ['Begin', 'Choose', 'Connection', 'Navigation',
-         'Next', 'Previous', 'Start', 'Stop', 'Hello', 'Web']
-
-# load the model, and pass in the custom metric function
-global graph
-graph = ops.get_default_graph()
-model = load_model(model_weights_path)
+@app.route('/')
+def index_view():
+    return render_template('index.html')
 
 
-@app.route("/load_model")
-def load_model():
-
-    model_is_loaded = False
-
-    """
-        Code for loading model
-    """
-
-    if model_is_loaded :
-        return{
-            "status" : "ok"
-        }
-
-    return {
-        "status" : "failed"
-    }
+def convertImage(imgData1):
+	imgstr = re.search(b'base64,(.*)', imgData1).group(1)
+	with open('output.png', 'wb') as output:
+	    output.write(base64.b64decode(imgstr))
 
 
-@app.route("/predict", methods=["POST"])
+def crop_and_save_image(img, img_path, write_img_path, img_name):
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(
+        'E:\Project_22\model\shape_predictor_81_face_landmarks.dat')
+    # load the input image, resize it, and convert it to grayscale
+
+    image = cv2.imread(img_path)
+    image = imutils.resize(image, width=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # detect faces in the grayscale image
+    rects = detector(gray, 1)
+    if len(rects) > 1:
+    	print("ERROR: more than one face detected")
+    	return
+    if len(rects) < 1:
+    	print("ERROR: no faces detected")
+    	return
+
+    for (i, rect) in enumerate(rects):
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+        name, i, j = 'mouth', 48, 68
+        clone = gray.copy()
+
+        (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
+        roi = gray[y:y+h, x:x+w]
+        roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
+        print('/content/gdrive/MyDrive/Lip Reading/cropped_small/' + write_img_path)
+        cv2.imwrite(
+            '/content/gdrive/MyDrive/Lip Reading/cropped_small/' + write_img_path, roi)
+
+
+@app.route('/predict/', methods=['GET', 'POST'])
 def predict():
+	imgData = request.get_data()
+	convertImage(imgData)
+	image = imageio.imread('output.png', mode='L')
+	image = resize(image, (MAX_WIDTH, MAX_HEIGHT))
+    image = 255 * image
+    image = np.expand_dims(image, axis=4)    
+
+	with graph.as_default():
+		out = model.predict(x)
+		print(out)
+		print(np.argmax(out, axis=1))
+
+		response = np.array_str(np.argmax(out, axis=1))
+		return response
 
 
-    requestFromFrontend = "array"
-
-
-    """
-       Code for Predict model
-    """
-    return{
-           "answers" : [
-                {
-                    "accuracy" : 99.5,
-                    "word" : "Dog"
-                },
-                {
-                    "accuracy" : 70,
-                    "word" : "God"
-                },
-                {
-                    "accuracy" : 9,
-                    "word" : "Mug"
-                }
-            ]
-        }
-
-
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
